a0c510bc3084923ac2ff5631d7121103e33942cb
Josiane705 <j.mukeshima@alustudent.com>
2025-02-21 07:48:32 +0000

    1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,7 +2,6 @@
 """Script that fetches 10 hot posts for a given subreddit."""
 import requests
 
-
 def top_ten(subreddit):
     """
     Print the titles of the top 10 hot posts for a given subreddit.
@@ -12,7 +11,9 @@ def top_ten(subreddit):
     headers = {'User-Agent': 'MyRedditBot/1.0 (by /u/yourusername)'}
     subreddit_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     params = {'limit': 10}  # Ensure we only get 10 posts
-    response = requests.get(subreddit_url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(
+        subreddit_url, headers=headers, params=params, allow_redirects=False
+    )
 
     if response.status_code == 200:
         json_data = response.json()
